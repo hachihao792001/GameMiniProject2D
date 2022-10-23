0fdfa6f1bdb8decbffe0ca636357b8446f4380d5
hachihao792001 <hachihao792001@gmail.com>
2022-10-23 15:36:06 +0700

    melee enemy walking animation

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class MeleeEnemy : Enemy
 {
     [SerializeField] Rigidbody2D _rb;
+    [SerializeField] SpriteRenderer _sr;
 
     void Update()
     {
         _rb.velocity = (player.transform.position - transform.position).normalized * _speed;
+        if(_rb.velocity.x != 0)
+        {
+            _sr.flipX = _rb.velocity.x < 0;
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
